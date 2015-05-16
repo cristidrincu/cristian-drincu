6b6f5f4810d90f04d1ddc69c4b40a45b10276e58
Cristian Sergiu Drincu <cristidrincu@gmail.com>
2015-05-16 22:20:33 +0300

    Refactored controller methods and routes

--- app/js/modules/portfolio/js/controllers.js
+++ app/js/modules/portfolio/js/controllers.js
@@ -1,37 +1,65 @@
+"use strict";
+
 angular.module('cristiandrincu.portfolio.controllers', [])
 	.controller('PortfolioController', ['$state', '$stateParams', '$scope', 'Portfolio', '$location', 'Lightbox', function ($state, $stateParams, $scope, Portfolio) {
 		$scope.loadingProjects = true;
 		$scope.portfolioType = $stateParams.type;
+		$scope.portfolioYear = $stateParams.year;
 
-		//you can use an {} to configure the query - Portfolio.query({limit: limit}) for example
-		Portfolio.query(function(projects){
-			var projects = _.filter(projects, function(project){
-				return project.published === $stateParams.year && project.type === $stateParams.type;
+
+		function init() {
+			//you can use an {} to configure the query - Portfolio.query({limit: limit}) for example
+			Portfolio.query(function(projects){
+				var projects = _.filter(projects, function(project){
+					return project.published === $stateParams.year && project.type === $stateParams.type;
+				});
+
+				$scope.projects = projects;
+				$scope.loadingProjects = false;
 			});
-
-			$scope.projects = projects;
-		  $scope.loadingProjects = false;
-		});
+		}
 
 		$scope.loadGraphicsProjects = function(){
-			$state.go('allGraphicsProjects.projectsGraphic2007', {year: '2007', type: 'graphic design'});
-		}
+			$state.go('allProjects.projects', {year: '2007', type: 'graphic design'});
+		};
 
 		$scope.loadWebDevelopmentProjects = function(){
-			$state.go('allProjects.projects2007', {year: '2007', type: 'web development'});
-		}
+			$state.go('allProjects.projects', {year: '2007', type: 'web development'});
+		};
 
 		$scope.loadPersonalProjects = function(){
-			$state.go('allPersonalProjects.personal2014', {year: '2014', type:'personal'});
-		}
+			$state.go('allProjects.projects', {year: '2014', type:'personal'});
+		};
+
+		init();
 
 	}])
 	.controller('PortfolioDetailsController', ['$stateParams', '$scope', 'Portfolio', 'Lightbox', function ($stateParams, $scope, Portfolio, Lightbox) {
+
 		$scope.loadingProjects = true;
-		$scope.portfolioDetail = Portfolio.get({id: $stateParams.id}, function(project){
-			$scope.loadingProjects = false;
-			return project;
-		});
+
+		function initProjectDetails () {
+			$scope.portfolioDetail = Portfolio.get({id: $stateParams.id}, function(project){
+				$scope.loadingProjects = false;
+				return project;
+			});
+		}
+
+		$scope.isPrevEnabled = function () {
+
+		};
+
+		$scope.isNextEnabled = function () {
+
+		};
+
+		$scope.gotoPrevProject = function () {
+
+		};
+
+		$scope.gotoNextProject = function () {
+
+		};
 
 		//this refers to the controller in our case. when using this inside a $scope function, this refers to the $scope
 		this.retrieveImages = function(){
@@ -49,5 +77,8 @@ angular.module('cristiandrincu.portfolio.controllers', [])
 
 		$scope.openLightboxModal = function(index){
 			Lightbox.openModal($scope.images, index);
-		}
+		};
+
+		initProjectDetails();
+
 	}]);
--- app/js/modules/portfolio/portfolioModule.js
+++ app/js/modules/portfolio/portfolioModule.js
@@ -1,94 +1,26 @@
+"use strict";
+
 angular.module('cristiandrincu.portfolioModule', [
 		'cristiandrincu.portfolio.services',
 		'cristiandrincu.portfolio.controllers',
 		'cristiandrincu.portfolio.directives',
 		'ui.router'
 	])
-	.config(['$stateProvider', '$locationProvider', function($stateProvider){
-
-		var constants = {
-				urlWebDevelopment : "/cristian-drincu/web-development",
-				urlGraphicDesign : "/cristian-drincu/graphic-design",
-				urlPersonalProjects: "/cristian-drincu/personal-projects"
-		}
-
-		$stateProvider.state('projectDetail', {
-			url: '/project-detail/:id',
-			templateUrl: 'js/modules/portfolio/views/portfolio-details.html',
-			controller: 'PortfolioDetailsController'
-		});
+	.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider){
 
 		$stateProvider.state('allProjects', {
 			url: '/portfolio',
 			template:"<ui-view></ui-view>",
 			abstract: true
-		}).state('allProjects.projects2007', {
-				url: constants.urlWebDevelopment + '/2007?year&type',
+		}).state('allProjects.projects', {
+				url: '/projects?year&type',
 				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
 				controller: 'PortfolioController'
-		}).state('allProjects.projects2008', {
-				url: constants.urlWebDevelopment + '/2008?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		}).state('allProjects.projects2009', {
-				url: constants.urlWebDevelopment + '/2009?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		}).state('allProjects.projects2010', {
-				url: constants.urlWebDevelopment + '/2010?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		}).state('allProjects.projects2011', {
-				url: constants.urlWebDevelopment + '/2011?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		}).state('allProjects.projects2012', {
-				url: constants.urlWebDevelopment + '/2012?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
+		}).state('allProjects.projectDetail', {
+				url: '/project-detail/:id',
+				templateUrl: 'js/modules/portfolio/views/portfolio-details.html',
+				controller: 'PortfolioDetailsController'
 		});
 
-		$stateProvider.state('allGraphicsProjects', {
-			url: '/projects-graphic-design?year&type',
-			template: '<ui-view></ui-view>',
-			abstract: true
-		}).state('allGraphicsProjects.projectsGraphic2007', {
-				url: constants.urlGraphicDesign  + '/2007?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		}).state('allGraphicsProjects.projectsGraphic2008', {
-				url: constants.urlGraphicDesign + '/2008?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		}).state('allGraphicsProjects.projectsGraphic2009', {
-				url: constants.urlGraphicDesign + '/2009?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		}).state('allGraphicsProjects.projectsGraphic2010', {
-				url: constants.urlGraphicDesign + '/2010?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		}).state('allGraphicsProjects.projectsGraphic2011', {
-				url: constants.urlGraphicDesign + '/2011?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		}).state('allGraphicsProjects.projectsGraphic2012', {
-				url: constants.urlGraphicDesign + '/2012?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		});
-
-		$stateProvider.state('allPersonalProjects', {
-			url: '/personal-projects?year&type',
-			template: '<ui-view></ui-view>',
-			abstract: true
-		}).state('allPersonalProjects.personal2014', {
-				url: constants.urlPersonalProjects + '/2014?year&type',
-				templateUrl: '/js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		}).state('allPersonalProjects.personal2015', {
-				url: constants.urlPersonalProjects + '/2015?year&type',
-				templateUrl: 'js/modules/portfolio/views/portfolio/portfolio-loading-template.html',
-				controller: 'PortfolioController'
-		});
+		$urlRouterProvider.otherwise("/portfolio/projects?year=2007&type=web development")
 	}]);
